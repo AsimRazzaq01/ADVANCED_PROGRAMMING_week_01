5769108edc0b5bc4ff3f9b5aff233b866e75459d
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-01-29 12:51:34 -0500

    create Constructor for Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -8,5 +8,12 @@ public class Vehicle {
     String fuelType ;
 
 
+    // Constructor that sets all attributes
+    public Vehicle(int numberOfWheels, String color, float engineSize, String fuelType) {
+        this.numberOfWheels = numberOfWheels;
+        this.color = color;
+        this.engineSize = engineSize;
+        this.fuelType = fuelType;
+    }
 
 }
