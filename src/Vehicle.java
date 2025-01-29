29808f1f3160a694482291ba1ddc90b28257d0fd
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-01-29 12:53:13 -0500

    create getter and setter methods for Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -16,4 +16,33 @@ public class Vehicle {
         this.fuelType = fuelType;
     }
 
+    //Getter Method
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+    public String getColor() {
+        return color;
+    }
+    public float getEngineSize() {
+        return engineSize;
+    }
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    //Setter Method
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setEngineSize(float engineSize) {
+        this.engineSize = engineSize;
+    }
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+
 }
