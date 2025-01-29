7f42f7be93a3dc553249e1277bf9b59fe85cd19a
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-01-29 13:12:49 -0500

    Instantiated a Vehicle object and a Car object. Tested the objects

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,23 @@
 public class Car extends Vehicle {
-
     String Brand ;
-    String Model ;
 
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String Brand) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.Brand = Brand;
+    }
 
+    //honk method
     public void honk(){
         System.out.println("Honk, honk! ");
     }
 
+    //display info method
     public void displayInfo(){
-        System.out.println(Brand);
-        System.out.println(Model);
+        System.out.println("This all the info about the: "+ Brand + " car !");
         System.out.println(numberOfWheels);
         System.out.println(color );
         System.out.println(engineSize);
         System.out.println(fuelType );
-        System.out.println();
-
         honk();
 
     }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        Vehicle motorbike = new Vehicle(2,"red", 2.14f,"Petrol");
+
+        Car McLaren = new Car(4,"Yellow", 12.6f,"Petrol", "McLadaren");
+
+
+        System.out.println("The motorbike has "+ motorbike.getNumberOfWheels()+ " wheels and is color " +
+                motorbike.color + " with and engine size of " +  motorbike.engineSize +" and takes "+ motorbike.fuelType + " fuel." );
+
+
+        McLaren.displayInfo();
 
     }
+
+
 }
